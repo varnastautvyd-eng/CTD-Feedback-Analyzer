6002e245d7eeb3d4dbf9e2bc5842e05a84535837
varnastautvyd-eng <varnastautvyd@gmail.com>
2025-11-19 16:54:06 +0200

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,6 +0,0 @@
-# Simplified placeholder streamlit_app.py
-import streamlit as st
-import pandas as pd
-
-st.title("CTD Feedback Analyzer - Simplified Template")
-st.write("Upload your Excel file below.")
